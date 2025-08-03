571ca369d901f7eaefe51c203af65157fdb38583
galvatron <dawnthivakar@gmail.com>
2025-08-03 23:28:10 +0530

    Review Controller updates

--- src/main/java/com/e_com/product/controller/ReiewController.java
+++ src/main/java/com/e_com/product/controller/ReiewController.java
@@ -3,10 +3,13 @@ package com.e_com.product.controller;
 import com.e_com.product.model.Review;
 import com.e_com.product.service.ReviewService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +28,15 @@ public class ReiewController {
     public ResponseEntity<List<Review>> getReviewsByProductId(Long productId) {
         List<Review> reviews = reviewService.getReviewsByProductId(productId);
         return ResponseEntity.ok(reviews);
+    }
+
+    @Operation(summary = "Add a new review",
+            description = "Create a new review for a product",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @PostMapping
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<Review> addReview(Review review) {
+        Review createdReview = reviewService.addReview(review);
+        return ResponseEntity.ok(createdReview);
     }
 }

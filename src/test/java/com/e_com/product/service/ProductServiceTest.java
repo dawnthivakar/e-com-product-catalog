f9b741c993b4b63adcf8ef58537ec49a695dcd08
galvatron <dawnthivakar@gmail.com>
2025-08-04 21:33:15 +0530

    Unit tests - Junit5

--- src/test/java/com/e_com/product/service/ProductServiceTest.java
+++ src/test/java/com/e_com/product/service/ProductServiceTest.java
@@ -22,8 +22,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@ActiveProfiles("test")
-@SpringBootTest
 class ProductServiceTest {
 
     @Mock
